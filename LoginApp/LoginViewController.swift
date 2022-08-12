f3e7927bcb9f8c063e9202cf0709659dbba13bd1
Anton Zarichny <antonzarichny@gmail.com>
2022-08-12 18:10:38 +0300

    1. Almost all

--- LoginApp/LoginViewController.swift
+++ LoginApp/LoginViewController.swift
@@ -20,14 +20,19 @@ class LoginViewController: UIViewController {
     
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true) // С методом из лекции почему-то не работает
+    }
+    
     override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
         userNameTF.text == userName && passwordTF.text == password
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
+        guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
+            welcomeVC.welcomeText = "Welcome, \(userName)!"
     }
-
+// MARK: - IBAction
     @IBAction func logInPressed() {
         if userNameTF.text != userName || passwordTF.text != password {
             showAlert(with: "Invalid login or password",
@@ -36,14 +41,21 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func forgotUserNamePressed() {
-        showAlert(with: "Oops!", and: "Your name is Tony")
+        showAlert(with: "Oops!", and: "Your name is \(userName)")
     }
     
     @IBAction func forgotPasswordPressed() {
-        showAlert(with: "Oops!", and: "Your password is 123")
+        showAlert(with: "Oops!", and: "Your password is \(password)")
+    }
+    
+    @IBAction func unwindSegue(_ segue: UIStoryboardSegue) {
+        userNameTF.text = ""
+        passwordTF.text = ""
     }
 }
-    
+
+
+// MARK: - Extension
 extension LoginViewController {
     private func showAlert(with title: String, and massage: String) {
         let alert = UIAlertController(title: title, message: massage, preferredStyle: .alert)
--- LoginApp/WelcomeViewController.swift
+++ LoginApp/WelcomeViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 class WelcomeViewController: UIViewController {
 
     @IBOutlet var welcomeLabel: UILabel!
+    
+    var welcomeText: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        welcomeLabel.text = welcomeText
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
